354dee8055d7f9f107f36f7713a56e013934ca0b
Zahan Malkani <zahanm@gmail.com>
2012-03-03 19:53:52 -0800

    printing first matches correctly

--- translate.py
+++ translate.py
@@ -2,18 +2,22 @@
 from __future__ import print_function
 
 import sys
-import urillib2
+import urllib2
 
 from bs4 import BeautifulSoup
+
+url = 'http://hindi-english.org/index.php?input={0}&trans=Translate&direction=AU'
 
 def word_translate(fwords):
   with open(fwords) as f, open('output','w') as out:
     for word in f:
-      site_stream = urillib2.urlopen(word.strip())
-      html = ''.join([ line for line in site_stream ])
-      soup = BeautifulSoup(html)
-      tbodies = soup.find_all(class='stil4')
-      print(tbodies[1])
+      if len(word.strip()):
+        site_stream = urllib2.urlopen(url.format(word.strip()))
+        html = ''.join([ line for line in site_stream ])
+        # print(html)
+        soup = BeautifulSoup(html)
+        tbodies = soup.find_all('a', { 'class': 'stil4' })
+        print(tbodies[1].get_text())
 
 if __name__ == '__main__':
   if len(sys.argv) != 2:
