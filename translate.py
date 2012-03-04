22b768d7531e5ecb2de33a7b5a4cb82ff3a974e2
Zahan Malkani <zahanm@gmail.com>
2012-03-03 19:36:54 -0800

    pulling urls

--- translate.py
+++ translate.py
@@ -1,3 +1,24 @@
 
-import http
+from __future__ import print_function
+
+import sys
+import urillib2
+
+from bs4 import BeautifulSoup
+
+def word_translate(fwords):
+  with open(fwords) as f, open('output','w') as out:
+    for word in f:
+      site_stream = urillib2.urlopen(word.strip())
+      html = ''.join([ line for line in site_stream ])
+      soup = BeautifulSoup(html)
+      tbodies = soup.find_all(class='stil4')
+      print(tbodies[1])
+
+if __name__ == '__main__':
+  if len(sys.argv) != 2:
+    print('usage: {0} <words_file>'.format(__file__))
+    sys.exit(1)
+  fname = sys.argv[1]
+  word_translate(fname)
 
